0479a618eaeef0e571959065740830e985beb815
Chris Kowalczyk <bachinaction@yahoo.ca>
2024-01-02 23:53:24 -0500

    Experimenting with Toggle.

--- frontend-quiz/src/App.jsx
+++ frontend-quiz/src/App.jsx
@@ -1,24 +1,36 @@
 
 import Button from './button.jsx';
-import './App.scss';
+import './App.css';
 import jsonData from './data.json';
-import Header from './HeaderText.jsx'
+import Header from './HeaderText.jsx';
+import Toggle from './Components/Toggle.jsx'
+import { useState } from "react";
+
 
 
 function App() {
+
+
+  const [toggle, setToggle] = useState(false);
+  const handleToggleChange = () => {
+    setToggle(!toggle);
+  };
   return (
     <div className="App">
-    <div> <Header /></div>
-     <div className='button-frame'>
-       {jsonData.quizzes.map((quiz, index) => (
-         <Button key={index} buttonText={quiz.title} />
-       ))}
-     </div>
-  </div>
+      <div style={toggle ? { backgroundColor: "black", color: "white" } : { backgroundColor: "white", color: "black" }}>
+        <Header />
+        <Toggle toggle={toggle} handleToggleChange={handleToggleChange} />
+        <div className='button-frame'>
+          {jsonData.quizzes.map((quiz, index) => (
+            <Button key={index} buttonText={quiz.title} />
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 
 export default App;
 
-
+// Take out Toggle code
 
