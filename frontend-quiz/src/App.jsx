8ef99be322a6e442fbd32d8d1a1d682f8b6e2c5b
Chris Kowalczyk <bachinaction@yahoo.ca>
2024-01-03 15:44:30 -0500

    Took out the code for the toggle will have to start over

--- frontend-quiz/src/App.jsx
+++ frontend-quiz/src/App.jsx
@@ -3,34 +3,29 @@ import Button from './button.jsx';
 import './App.css';
 import jsonData from './data.json';
 import Header from './HeaderText.jsx';
-import Toggle from './Components/Toggle.jsx'
-import { useState } from "react";
+
 
 
 
 function App() {
 
 
-  const [toggle, setToggle] = useState(false);
-  const handleToggleChange = () => {
-    setToggle(!toggle);
-  };
   return (
     <div className="App">
-      <div style={toggle ? { backgroundColor: "black", color: "white" } : { backgroundColor: "white", color: "black" }}>
+      
         <Header />
-        <Toggle toggle={toggle} handleToggleChange={handleToggleChange} />
+       
         <div className='button-frame'>
           {jsonData.quizzes.map((quiz, index) => (
             <Button key={index} buttonText={quiz.title} />
           ))}
         </div>
       </div>
-    </div>
+    
   );
 }
 
 export default App;
 
-// Take out Toggle code
+// Take out Toggle code based on this article: https://www.educative.io/answers/how-to-use-the-toggle-component-in-react
 
--- frontend-quiz/src/Components/Toggle.jsx
+++ frontend-quiz/src/Components/Toggle.jsx
@@ -1,25 +1,3 @@
 
-import React from "react";
-import "./Toggle.css"; 
-
-const Toggle = ({ toggle, handleToggleChange }) => {
-  return (
-    <>
-        <div className="t1">Turn {toggle ? 'off' : 'on'} the dark mode</div>
-       
-        <div className='toggle-container' onClick={handleToggleChange}>
-            <div className={`toggle-btn ${!toggle ? "disable" : ""}`}>
-                {toggle ? "ON" : "OFF"}
-            </div>
-        </div>
-
-        <span className="emo">{toggle ? "🦉" : "🌤️"}</span>       
-    </>
-   
-  );
-};
-
-export default Toggle;
-
 
 
